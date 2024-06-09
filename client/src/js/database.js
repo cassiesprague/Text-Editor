1e5edd70af9357d613f7984388decd3b42b895a9
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-06-08 20:58:31 -0400

    added info to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Creates database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +14,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  await tx.done;
+  console.log('Content added to database:', content);
+};
+
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = [];
+  await store.openCursor().then(function cursorIterate(cursor) {
+    if (!cursor) return;
+    allContent.push(cursor.value);
+    return cursor.continue().then(cursorIterate);
+  });
+  return allContent;
+};
+
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
